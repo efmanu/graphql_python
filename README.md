b2be631aea690db67e5b2dc1299e063969acf989
Manu <manuedavakandam@gmail.com>
2024-04-23 23:04:14 +1000

    feat/gql_mutation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Example code to create graphql implementation with python
 
 
-# GQL With Graphene + SQLAlchemy + Flask
+# GQL With Graphene + SQLAlchemy + Flask + Alembic
 
 # Setup postgres db using docker 
 ```
@@ -29,4 +29,78 @@ pip install graphene graphene-sqlalchemy flask sqlalchemy alembic uuid flask_gra
 If you are using Mac ARM processor
 ```
 pip install psycopg2-binary --force-reinstall --no-cache-dir
+```
+
+# DB Migration
+# Init Alembic
+```
+alembic init alembic
+```
+
+# Update alembic.ini file
+This is to update connection details
+```
+sqlalchemy.url = postgresql://root:password@localhost:5466/testdb
+```
+
+# Update alembic/env.py file
+This is to insert the Base model. Import Base and update the target_metadata
+```
+from models import Base
+
+
+target_metadata = Base.metadata
+```
+
+# Create Revison and migrate
+Replace message with custom messages related to each revision
+```
+alembic revision -m "intial migration"
+
+alembic upgrade head
+```
+
+OR
+
+```
+alembic-autogen-check || alembic revision --autogenerate -m 'Add new updates'
+alembic upgrade head
+```
+
+
+# Run APp
+
+```
+python3 server.py
+```
+
+
+# Sample graphQL query and Mutations:
+
+Open [http://127.0.0.1:5000/graphql](http://127.0.0.1:5000/graphql?operationName=cre&query=mutation%20cre%7B%0A%20%20createUser(userDetails%3A%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20username%3A%22manu123%22%0A%20%20%20%20%20%20firstName%3A%22manu%22%0A%20%20%20%20%20%20lastName%3A%22Francis%22%0A%20%20%20%20%7D%0A%20%20%5D)%7B%0A%20%20%20%20users%7B%0A%20%20%20%20%20%20userId%0A%20%20%20%20%20%20username%0A%20%20%20%20%20%20firstName%0A%20%20%20%20%20%20lastName%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A%0Aquery%20liU%7B%0A%20%20getUser(username%3A%22manu123%22)%7B%0A%20%20%20%20username%0A%20%20%20%20firstName%0A%20%20%20%20lastName%0A%20%20%7D%0A%7D) in browser
+``` 
+mutation cre{
+  createUser(userDetails:[
+    {
+      username:"manu123"
+      firstName:"manu"
+      lastName:"Francis"
+    }
+  ]){
+    users{
+      userId
+      username
+      firstName
+      lastName
+    }
+  }
+}
+
+query liU{
+  getUser(username:"manu123"){
+    username
+    firstName
+    lastName
+  }
+}
 ```
--- schema/schema.py
+++ schema/schema.py
@@ -10,11 +10,47 @@ from graphene_sqlalchemy.registry import get_global_registry
 from graphene_sqlalchemy.types import construct_fields
 from graphene_sqlalchemy.fields import default_connection_field_factory
 
+from sqlalchemy.orm import defer
+
 from db.model import (
     User as UserModel
 )
 
 
+
+class SQLAlchemyInputObjectType(graphene.InputObjectType):
+    """ 
+    To Generate Graphene Mutation Inputs from SQLAlchemy Class Attributes
+    https://stackoverflow.com/questions/48806710/generate-graphene-mutation-inputs-from-sqlalchemy-class-attributes
+
+    """
+    @classmethod
+    def __init_subclass_with_meta__(  # pylint: disable=arguments-differ
+        cls, obj_type=None, model=None, registry=None, only_fields=(), exclude_fields=(),batching=False,
+        optional_fields=(), **options
+    ):
+        if not registry:
+            registry = get_global_registry()
+
+        connection_field_factory = default_connection_field_factory
+        
+        sqla_fields = yank_fields_from_attrs(
+            construct_fields(obj_type, model, registry, only_fields, exclude_fields, batching, connection_field_factory),
+            _as=graphene.Field,
+        )
+
+        for key, value in sqla_fields.items():
+            if key in optional_fields:
+                type_ = value.type if isinstance(
+                    value.type, SubclassWithMeta_Meta) else value.type.of_type
+                value = type_(
+                    description=value.description
+                )
+            setattr(cls, key, value)
+
+        super(SQLAlchemyInputObjectType, cls).__init_subclass_with_meta__(
+            **options
+        )
 
 
 class UserObject(SQLAlchemyObjectType):
@@ -22,15 +58,49 @@ class UserObject(SQLAlchemyObjectType):
         model = UserModel
         interfaces = (relay.Node,)
 
+
 class Query(graphene.ObjectType):
     getUser = graphene.List(UserObject, username=graphene.String())
 
     def resolve_getUser(root, info, username=None):
         query = session.query(UserModel)
         if username is not None:
-            return query.filter(UserModel.username == username)
+            return query.filter(UserModel.username == username).all()
         return query.all()
 
 
+class CreateUserInput(SQLAlchemyInputObjectType):
+    class Meta:
+        model = UserModel
+        interfaces = (relay.Node,)
+        obj_type=UserObject
+        exclude_fields = (
+            'user_id',
+            'id'
+        )
 
-schema = Schema(query=Query)
+
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        user_details = graphene.List(CreateUserInput)
+
+    users = graphene.List(UserObject)
+
+    def mutate(self, info, user_details):
+
+        new_users = []
+        for user in user_details:
+            new_user = UserModel(username=user.username, user_id=f"USER&&ID&&{str(uuid.uuid4())}", first_name=user.first_name, last_name=user.last_name)
+            session.add(new_user)
+            new_users.append(new_user)
+
+        session.commit()
+
+        return CreateUser(users=new_users)
+
+
+class Mutation(graphene.ObjectType):\
+    create_user = CreateUser.Field()
+
+
+schema = Schema(query=Query, mutation=Mutation)
